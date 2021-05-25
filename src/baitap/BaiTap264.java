3e4e39a08c0ab9946fc8037d8a4e063c811a391d
nguyendinhquochuy <nguyendinhquochuy0311@gmail.com>
2021-05-25 09:11:40 +0700

    feat: add class BaiTap264

---
+++ src/baitap/BaiTap264.java
@@ -0,0 +1,12 @@
+package baitap;
+
+public class BaiTap264 {
+    public static void main(String[] args) {
+//        Bài 264: Trộn 2 mảng đã tăng thành 1 mảng được sắp xếp tăng
+
+        BaiTap10000000 baiTap10000000 = new BaiTap10000000();
+
+        String s = baiTap10000000.getA();
+        System.out.println(s);
+    }
+}
