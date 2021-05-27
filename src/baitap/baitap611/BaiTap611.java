f8cb94b304d0e6e12f60cd69ba12592474c21ab2
nguyendinhquochuy <nguyendinhquochuy0311@gmail.com>
2021-05-27 22:05:03 +0700

    feat: add BaiTap611

--- src/baitap/baitap611/BaiTap611.java
+++ src/baitap/baitap611/BaiTap611.java
@@ -1,10 +1,13 @@
 package baitap.baitap611;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 
 public class BaiTap611 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 //        Bài 611: Hãy khai báo kiểu dữ liệu để biểu diễn thông tin của một chuyến bay. Biết rằng một chuyến bay gồm những thành phần sau:
 //
 //        Mã chuyến bay: chuỗi tối đa 5 ký tự
@@ -13,28 +16,38 @@ public class BaiTap611 {
 //        Nơi đi: chuỗi tối đa 20 ký tự
 //        Nơi đến: chuỗi tối đa 20 ký tự
 //        Sau đó viết hàm nhập, xuất cho kiểu dữ liệu này
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhập Mã chuyến bay");
-        String flightCode = scanner.nextLine();
-        int sizeFlightCode = String.valueOf(flightCode).length();
-        if (sizeFlightCode > 5) {
-            System.out.println("Mã chuyến bay không được quá 5 kí tự");
-        }
+        int numberCustomer;
+        System.out.println("flight");
 
-        System.out.println("Nhập ngày bay");
-        int flightDate = scanner.nextInt();
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Nhập Mã chuyến bay");
+//        String flightCode = scanner.nextLine();
+//
+//        System.out.println("Nhap ngay bay");
+//        String flightDate = scanner.nextLine();
 
-        System.out.println("Nhập tháng bay");
-        int flightMonth = scanner.nextInt();
+        String dateString = "6/5/2017";
 
-        System.out.println("Nhập năm bay");
-        int flightYear = scanner.nextInt();
+        SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        df.setLenient(false);
 
-        System.out.println(" Ngày bay:" + flightDate + "/" + flightMonth + "/" + flightYear);
+        Date date = null;
 
-        Calendar date = Calendar.getInstance();
+        date = df.parse(dateString);
+
+        System.out.println(date);
 
 
+//        int sizeFlightCode = String.valueOf(flightCode).length();
+//        if (sizeFlightCode > 5) {
+//            System.out.println("Mã chuyến bay không được quá 5 kí tự");
+//        }
 
+        FlightInformation flightInformation = new FlightInformation();
+        FlightInformation flightInformations = new FlightInformation();
+//        /asdsadasdsadsa
+        flightInformations.toString();
+
+        int a[] = {112};
     }
 }
--- src/baitap/baitap611/FlightInformation.java
+++ src/baitap/baitap611/FlightInformation.java
@@ -2,5 +2,75 @@ package baitap.baitap611;
 
 public class FlightInformation {
 
+    private String flightCode;
 
+    private Long flightDate;
+
+    private Long flightTime;
+
+    private String departure;
+
+    private String destination;
+
+    public FlightInformation() {
+    }
+
+    public String getFlightCode() {
+        return flightCode;
+    }
+
+    public void setFlightCode(String flightCode) {
+        this.flightCode = flightCode;
+    }
+
+    public Long getFlightDate() {
+        return flightDate;
+    }
+
+    public FlightInformation(String flightCode, Long flightDate, Long flightTime, String departure, String destination) {
+        this.flightCode = flightCode;
+        this.flightDate = flightDate;
+        this.flightTime = flightTime;
+        this.departure = departure;
+        this.destination = destination;
+    }
+
+    public void setFlightDate(Long flightDate) {
+        this.flightDate = flightDate;
+    }
+
+    public Long getFlightTime() {
+        return flightTime;
+    }
+
+    public void setFlightTime(Long flightTime) {
+        this.flightTime = flightTime;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(String departure) {
+        this.departure = departure;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    @Override
+    public String toString() {
+        return "FlightInformation{" +
+                "flightCode='" + flightCode + '\'' +
+                ", flightDate=" + flightDate +
+                ", flightTime=" + flightTime +
+                ", departure='" + departure + '\'' +
+                ", destination='" + destination + '\'' +
+                '}';
+    }
 }
