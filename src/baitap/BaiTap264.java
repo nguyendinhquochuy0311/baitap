0f1780039be73f12d7fe2847aaaa6d4d05b8de97
nguyendinhquochuy <nguyendinhquochuy0311@gmail.com>
2021-05-28 10:15:50 +0700

    feat: fix tonight

---
+++ src/baitap/BaiTap264.java
@@ -0,0 +1,13 @@
+package baitap;
+
+public class BaiTap264 {
+    public static void main(String[] args) {
+//        Bài 264: Trộn 2 mảng đã tăng thành 1 mảng được sắp xếp tăng
+        int[] mang1 = {2, 3, 5, 6};
+        int[] mang2 = {0, 1, 4,};
+
+        int[] mangTron = new int[mang1 + mang2];
+
+    }
+
+}
