52188eb216d0253fd12abc268bd4c4b3496b1291
nguyendinhquochuy <nguyendinhquochuy0311@gmail.com>
2021-06-05 00:13:04 +0700

    feat: Validate Data Input BaiTap311And313

--- src/baitap/BaiTap311And313.java
+++ src/baitap/BaiTap311And313.java
@@ -3,31 +3,90 @@ package baitap;
 import java.util.Scanner;
 
 public class BaiTap311And313 {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println(" nhap so cot");
-        int columNumber = scanner.nextInt();
 
-        System.out.println("nhap so hang");
-        int rowNumber = scanner.nextInt();
+        boolean checkType;
+        String columnNumberStr;
+        System.out.println("input count column");
 
-        int[][] matrix = new int[rowNumber][columNumber];
+        do {
+            columnNumberStr = scanner.nextLine();
+            checkType = isNumeric(columnNumberStr);
+        } while (!checkType);
 
+        int columnNumber = Integer.parseInt(columnNumberStr);
+
+        String rowNumberStr;
+        System.out.println("input count row");
+        do {
+            rowNumberStr = scanner.nextLine();
+            checkType = isNumeric(rowNumberStr);
+        } while (!checkType);
+
+        int rowNumber = Integer.parseInt(rowNumberStr);
+
+        double[][] matrix = new double[rowNumber][columnNumber];
+
+        boolean checkNumber;
+        String number;
+        double numberMatrix;
 
         for (int i = 0; i < rowNumber; i++) {
-            for (int j = 0; j < columNumber; j++) {
-                System.out.println("nhap toa do: [" + i + "],[" + j + "]: ");
-                matrix[i][j] = scanner.nextInt();
+            for (int j = 0; j < columnNumber; j++) {
+                System.out.println("nhap gia tri cua toa do: [" + i + "],[" + j + "]: ");
+                do {
+                    number = scanner.nextLine();
+                    checkNumber = isNumericData(number);
+
+                } while (!checkNumber);
+                numberMatrix = Double.parseDouble(number);
+                matrix[i][j] = numberMatrix;
             }
         }
 
         System.out.println("Mang vua nhap");
         for (int i = 0; i < rowNumber; i++) {
-            for (int j = 0; j < columNumber; j++) {
+            for (int j = 0; j < columnNumber; j++) {
                 System.out.print(matrix[i][j] + "\t");
             }
             System.out.println();
         }
     }
 
+    public static boolean isNumericData(String strNum) {
+
+        if (strNum == null) {
+            System.out.println("retype");
+            return false;
+        }
+        try {
+            double numberData = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            System.out.println("retype");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isNumeric(String strNum) {
+
+        if (strNum == null) {
+            System.out.println("reType");
+            return false;
+        }
+        try {
+            int number = Integer.parseInt(strNum);
+            if (number <= 0) {
+                System.out.println("reType");
+                return false;
+            }
+        } catch (NumberFormatException nfe) {
+            System.out.println("reType");
+            return false;
+        }
+        return true;
+    }
 }
