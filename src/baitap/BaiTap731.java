ab33b5767e15dd5a19bb2e8c3736a0b047407799
nguyendinhquochuy <nguyendinhquochuy0311@gmail.com>
2021-06-07 18:09:34 +0700

    update

--- src/baitap/BaiTap731.java
+++ src/baitap/BaiTap731.java
@@ -7,10 +7,18 @@ public class BaiTap731 {
     public static void main(String[] args) {
 
 
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("nhap n");
-        int n = scanner.nextInt();
+
+        boolean checkType;
+        String numberCheck;
+
+        do {
+            numberCheck = scanner.nextLine();
+            checkType = Common.isNumeric(numberCheck);
+        } while (!checkType);
+
+        int n = Integer.parseInt(numberCheck);
         int s = 0;
         for (int i = 1; i <= n; i++) {
             s += Math.pow(i, 2);
