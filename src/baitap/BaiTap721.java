d0134bc07138c3b8fc34e26b7ade2e028ca61a28
nguyendinhquochuy <nguyendinhquochuy0311@gmail.com>
2021-06-10 14:57:48 +0700

    feat: clean code

--- src/baitap/BaiTap721.java
+++ src/baitap/BaiTap721.java
@@ -6,7 +6,7 @@ public class BaiTap721 {
     //    Bài 732: Tính S(n) = 1 + ½ + 1/3 + … + 1/n
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("nhap n");
+        System.out.println("input number");
 
         boolean checkType;
         String numberCheck;
@@ -15,12 +15,11 @@ public class BaiTap721 {
             checkType = Common.isNumeric(numberCheck);
         } while (!checkType);
 
-        int n = Integer.parseInt(numberCheck);
-        double s = 0;
-        for (int i = 1; i <= n; i++) {
-            s += (double) 1 / i;
+        int inputNumber = Integer.parseInt(numberCheck);
+        double sum = 0;
+        for (int i = 1; i <= inputNumber; i++) {
+            sum += (double) 1 / i;
         }
-        System.out.println(s);
-
+        System.out.println(sum);
     }
 }
