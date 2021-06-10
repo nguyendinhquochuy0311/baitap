609adbceb6342971201c61fb641a2d61db66e169
nguyendinhquochuy <nguyendinhquochuy0311@gmail.com>
2021-06-10 15:26:24 +0700

    feat: update code

--- src/baitap/Fibonacci.java
+++ src/baitap/Fibonacci.java
@@ -5,19 +5,31 @@ import java.util.Scanner;
 public class Fibonacci {
     //    Viết chương trình c tìm n số Fibonacci đầu tiên.
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("nhap so chu so dau tien");
-        int n = scanner.nextInt();
-        int number1 = 0;
-        int number2 = 1;
+
+        boolean checkType;
+        String numberCheck;
+        do {
+            numberCheck = scanner.nextLine();
+            checkType = Common.isNumeric(numberCheck);
+        } while (!checkType);
+
+        int inputNumber = Integer.parseInt(numberCheck);
+
+        int beforeNumber = 0;
+        int afterNumber = 1;
         int sumOfNumber;
-        System.out.println(number1);
-        System.out.println(number2);
-        for (int i = 0; i < (n - 2); i++) {
-            sumOfNumber = number1 + number2;
-            number1 = number2;
-            number2 = sumOfNumber;
+
+        for (int i = 0; i < inputNumber; i++) {
+
+            sumOfNumber = beforeNumber + afterNumber;
             System.out.println(sumOfNumber);
+
+            afterNumber = beforeNumber;
+            beforeNumber = sumOfNumber;
+
         }
     }
 }
